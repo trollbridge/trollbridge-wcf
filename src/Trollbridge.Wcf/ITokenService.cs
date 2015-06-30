e72cfe069afc17a966fd59124174d3b730a02109
adam grocholski <adgroc@outlook.com>
2015-06-30 15:36:58 -0500

    ugh, git fail

---
+++ src/Trollbridge.Wcf/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using Trollbridge.Core;
+
+namespace Trollbridge.Wcf
+{
+    [ServiceContract]
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Gets SAS tokens to enable a device to access Azure blobs, queues, and event hubs.
+        /// </summary>
+        /// <param name="id">Id of device to return SAS tokens for.</param>
+        /// <param name="compressed">Whether or not to compress the returned tokens.</param>
+        /// <returns></returns>
+        [OperationContract]
+        TokenHolder GetTokenCompressed(string id, bool compressed);
+
+    }
+}
---
+++ src/Trollbridge.Wcf/TokenService.svc.cs
@@ -0,0 +1,22 @@
+﻿using Trollbridge.Core;
+
+namespace Trollbridge.Wcf
+{
+    public class TokenService : ITokenService
+    {
+        /// <summary>
+        /// Gets SAS tokens to enable a device to access Azure blobs, queues, and event hubs.
+        /// </summary>
+        /// <param name="id">Id of device to return SAS tokens for.</param>
+        /// <param name="compressed">Whether or not to compress the returned tokens.</param>
+        /// <returns></returns>
+        public TokenHolder GetTokenCompressed(string id, bool compressed)
+        {
+            if (compressed)
+                return SasTokenHelper.GetTokensCompressed(id);
+            else
+                return SasTokenHelper.GetTokensUncompressed(id);
+        }
+
+    }
+}
